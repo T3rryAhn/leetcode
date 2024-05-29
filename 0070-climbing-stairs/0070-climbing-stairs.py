64f55ad3b099634b074047b6b92de2cbd67bcebf
T3rry <t3rryahn.dev@gmail.com>
2024-05-29 22:09:06 +0900

    Time: 37 ms (38.34%), Space: 16.5 MB (81.27%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        step = [0] * 45
+        step[0] = 1
+        step[1] = 2
+        for i in range(2, n):
+            step[i] = step[i - 1] + step[i - 2]
+        return step[n - 1]
