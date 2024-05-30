95bf93a27b76f254afe28a79c256fc502d9890c6
T3rry <t3rryahn.dev@gmail.com>
2024-05-30 14:24:53 +0900

    Time: 1001 ms (7.14%), Space: 29.6 MB (75%) - LeetHub

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.py
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.py
@@ -7,17 +7,16 @@ class Solution:
         for i in range(1, len(nums)):
             if front[i] < front[i - 1]:
                 front[i] = front[i - 1]
-            else:
-                front[i] = front[i]
 
         for i in range(len(nums) - 2, -1, -1):
             if back[i] < back[i + 1]:
                 back[i] = back[i + 1]
-            else:
-                back[i] = back[i]
 
         for i in range(1, len(nums) - 1):
-            temp_answer = (front[i] - nums[i]) * back[i]
+            temp_answer = (front[i - 1] - nums[i]) * back[i + 1]
+            print(temp_answer)
             answer = max(answer, temp_answer)
+        print(front)
+        print(back)
 
         return answer
