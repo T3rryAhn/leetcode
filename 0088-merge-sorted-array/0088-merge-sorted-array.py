00735193b4ff8745f14fbfff703f8e6d42d6842a
T3rry <t3rryahn.dev@gmail.com>
2025-01-10 16:00:39 +0900

    Time: 0 ms (100%), Space: 17.7 MB (35.82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,7 +1,17 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        for i in range(n):
-            nums1[m + i] = nums2[i]
+        p1 = m - 1 # nums1의 마지막 유효 원소의 인덱스
+        p2 = n - 1 # nums2의 마지막 원소의 인덱스
+        p = m + n - 1 # nums1의 마지막 인덱스
 
-        nums1.sort()
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            
+            p -= 1
+
         
